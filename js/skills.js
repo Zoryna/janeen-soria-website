9abb679c03aeaf0e758ff3509b1f66b8337a509a
Zoryna <jsoryna@gmail.com>
2020-11-29 22:32:51 -0600

    Used for loop for skills eventListener

--- js/skills.js
+++ js/skills.js
@@ -1,39 +1,11 @@
 //when mouse hovers, current text disappears and other text appears
 
-//https://stackoverflow.com/questions/48610654/how-to-make-a-button-disappear-and-reappear-after-x-seconds
-//https://www.w3schools.com/jsref/event_onmouseenter.asp
-
-//do a for loop for each skill?
-document.getElementById("javaskill").addEventListener("mouseenter", showExperience)
-document.getElementById("javaskill").addEventListener("mouseleave", showSkill)
-
-document.getElementById("htmlskill").addEventListener("mouseenter", showExperience)
-document.getElementById("htmlskill").addEventListener("mouseleave", showSkill)
-
-document.getElementById("cssskill").addEventListener("mouseenter", showExperience)
-document.getElementById("cssskill").addEventListener("mouseleave", showSkill)
-
-document.getElementById("gitskill").addEventListener("mouseenter", showExperience)
-document.getElementById("gitskill").addEventListener("mouseleave", showSkill)
-
 function showExperience()
 {
   document.getElementById("javaskill").innerHTML = "5000+ lines"
   document.getElementById("htmlskill").innerHTML = "5000+ lines"
   document.getElementById("cssskill").innerHTML = "1000+ lines"
   document.getElementById("gitskill").innerHTML = "2 years"
-
-
-  /*var experience = document.getElementByTagName("skill")
-  for (i = 0; i <= experience.length; i++)
-  {
-    document.getElementById("skill").innerHTML = test
-  } */
-
-
-
-
-
 }
 
 function showSkill()
@@ -42,19 +14,18 @@ function showSkill()
   document.getElementById("htmlskill").innerHTML = "HTML"
   document.getElementById("cssskill").innerHTML = "CSS"
   document.getElementById("gitskill").innerHTML = "Git"
-
-
-  /*var skill = document.getElementByTagName("skill")
-  for (i = 0; i <= skill.length; i++)
-  {
-    document.getElementById("skill").innerHTML = test
-  } */
-
-  //document.getElementById("skill").style.color = "red"
-  //getElementsByTagName
-
-
-
-
-
 }
+
+function hoverExperience()
+{
+  var idArray = ["javaskill", "htmlskill", "cssskill", "gitskill"]
+  for (var i = 0; i < idArray.length; i++)
+  {
+    document.getElementById(idArray[i]).addEventListener("mouseenter", showExperience)
+    document.getElementById(idArray[i]).addEventListener("mouseleave", showSkill)
+  }
+}
+
+showExperience()
+showSkill()
+hoverExperience()
